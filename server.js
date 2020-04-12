adadf8be1f3da2a83bdb100cf17834614c855bc2
arianrah <arianrahimi@icloud.com>
2020-04-11 19:44:33 -0700

    created route files.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 1337
 //Connect to atlas
 connectDB()
 
-app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
+//Def Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
 
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
 app.get('/', (req, res) => res.send('API is A-Okay!'))

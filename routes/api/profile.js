6b9279aeab7185f713cf0bd0139bd6587c0f4833
arianrah <arianrahimi@icloud.com>
2020-04-12 22:59:13 -0700

    @route POST api/profile completed.. so.. many... fields.....

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
 
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -24,5 +25,67 @@ router.get('/me', auth, async (req, res) => {
 		res.status(500).send('Server Error')
 	}
 })
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  private
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required')
+				.not()
+				.isEmpty(),
+			check('skills', 'Skills is required')
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubuser,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin
+		} = req.body
+
+		//build profile object + check
+		const profileFields = {}
+		profileFields.user = req.user.id
+		if (company) profileFields.company = company
+		if (website) profileFields.website = website
+		if (location) profileFields.location = location
+		if (bio) profileFields.bio = bio
+		if (status) profileFields.status = status
+		if (githubuser) profileFields.githubuser = githubuser
+		if (skills) {
+			profileFields.skills = skills.split(',').map(skill => skill.trim())
+		}
+		// console.log(profileFields.skills)
+		// res.send('Hit')
+
+		//build social object
+		profileFields.social = {}
+		if (youtube) profileFields.social.youtube = youtube
+		if (facebook) profileFields.social.facebook = facebook
+		if (twitter) profileFields.social.twitter = twitter
+		if (instagram) profileFields.social.instagram = instagram
+		if (linkedin) profileFields.social.linkedin = linkedin
+	}
+)
 
 module.exports = router

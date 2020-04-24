7116877803553670d591dc9d62fe14d6c9c4da81
arianrah <arianrahimi@icloud.com>
2020-04-24 12:30:18 -0700

    heroku init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 1337
@@ -16,5 +17,14 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/auth', require('./routes/api/auth'))
 
+//serve static assets for productions
+if (process.env.NODE_ENV === 'production') {
+	// set staic folder
+	app.use(express.static('client/build'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
-app.get('/', (req, res) => res.send('API is A-Okay!'))
